ed8cee3b8a46f405b64e3e28a50c518ec8db2acd
nesrine <nesrine.aissaoui@esprit.tn>
2022-11-01 10:24:45 +0100

    connection.h

--- connection.h
+++ connection.h
@@ -1,11 +1,17 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
 
 
 class connection
 {
+    QSqlDatabase db ;
 public:
     connection();
+    bool createconnection();
+    void closeConnection();
 };
 
 #endif // CONNECTION_H
