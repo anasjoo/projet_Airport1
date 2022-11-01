c0ba0595d4b11972aad6d1b398b9d01ff757131e
nesrine <nesrine.aissaoui@esprit.tn>
2022-11-01 10:23:58 +0100

    connection.cpp

--- connection.cpp
+++ connection.cpp
@@ -1,6 +1,23 @@
 #include "connection.h"
+#include"mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
 
 connection::connection()
 {
 
+}
+bool connection::createconnection()
+{
+    db = QSqlDatabase::addDatabase("QODBC");
+    bool test=false;
+    db.setDatabaseName("SMART_AIRPORT");
+    db.setUserName("Nesrine");
+    db.setPassword("esprit18");
+    if (db.open()) test=true;
+    return test;
+}
+void connection::closeConnection()
+{
+    db.close();
 }
