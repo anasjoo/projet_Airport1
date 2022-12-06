678aabf77f5de205ed58f3726020549534eab606
Nesrine <nesrine.aissaoui@esprit.tn>
2022-12-06 07:56:39 +0100

    vol.h

--- vol.h
+++ vol.h
@@ -3,26 +3,34 @@
 # include <QString>
 #include <QSqlQueryModel>
 #include <QSqlQuery>
+//#include "mainwindow.h"
+//#include "ui_mainwindow.h"
+
 class Vol
 {
 public:
     Vol();
-    Vol (int, QString,QString,QString);
-    int getID_VOL();
+    Vol (QString, QString,QString,QString);
+    QString  getID_VOL();
     QString getDIRECTION();
     QString getARRIVE_VOL();
     QString getDEPART_VOL();
-    void setID_VOL(int);
+    void setID_VOL(QString);
     void setDIRECTION(QString);
     void setARRIVE_VOL(QString);
     void setDEPART_VOL(QString);
     bool ajouter();
     QSqlQueryModel* afficher();
-    bool supprimer(int);
-    bool modifier (int);
-private:
-int ID_VOL ;
-QString ARRIVE_VOL,DEPART_VOL, DIRECTION;
+    bool supprimer(QString);
+    bool modifier (QString);
+    QSqlQueryModel * rechercher(QString);
+     QSqlQueryModel *tri();
+     QSqlQueryModel * trid();
+     bool modifier_emploi(QString ID_VOL, QString DEPART_VOL, QString ARRIVE_VOL);
+
+     bool ajouterMessage(QString);
+     QSqlQueryModel * afficherMessage();
+QString ARRIVE_VOL,DEPART_VOL,ID_VOL, DIRECTION;
 };
 
 #endif // VOL_H
